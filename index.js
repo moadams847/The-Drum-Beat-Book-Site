eecc1e897bc02d0e2971e73f7343b781b450c8e7
Adams Mohammed <moadams847@gmail.com>
2021-07-30 23:28:31 +0000

    create and update js file

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-// initialize bootstrap tooltip----------------------------
+// variables----------------
+var btnJs = document.querySelectorAll(".btnJs");
+for (var i = 0; i < btnJs.length; i++) {
+  btnJs[i].addEventListener("click", function (e) {
+    if (e.target.classList.contains("myBtn-1")) {
+      readMoreLess("dots-1", "more-1", "btnId-1");
+    } else if (e.target.classList.contains("myBtn-2")) {
+      readMoreLess("dots-2", "more-2", "btnId-2");
+    }
+  });
+}
+
+// fxn---------------
+function readMoreLess(dots, text, btn) {
+  var dots = document.getElementById(dots);
+  var moreText = document.getElementById(text);
+  var btnText = document.getElementById(btn);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// initialize tooltip
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
